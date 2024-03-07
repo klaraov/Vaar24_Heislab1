659b98094290d88328da5bf567649ee0084396ec
selmaja <112476021+selmaja@users.noreply.github.com>
2024-03-07 12:28:39 +0100

    Update main.c

--- lab_2-0.1.0/skeleton_project/source/main.c
+++ lab_2-0.1.0/skeleton_project/source/main.c
@@ -4,61 +4,88 @@
 #include <time.h>
 #include "driver/elevio.h"
 
+struct Request{
+    int floorTo;
+    int direction;
+};
 
+struct Request rlist [];
 
-int main(){ 
+int main(){
     elevio_init();
-    
+
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
 
     elevio_motorDirection(DIRN_UP);
 
     while(1){
+
         int floor = elevio_floorSensor();
 
         if(floor == 0){
             elevio_motorDirection(DIRN_UP);
+            elevio_floorIndicator(0);
         }
-
+        if(floor == 1){
+            elevio_floorIndicator(1);
+        }
+        if(floor == 2){
+            elevio_floorIndicator(2);
+        }
         if(floor == N_FLOORS-1){
             elevio_motorDirection(DIRN_DOWN);
+            elevio_floorIndicator(3);
         }
-        
-        
 
         for(int f = 0; f < N_FLOORS; f++){
             for(int b = 0; b < N_BUTTONS; b++){
+
                 int btnPressed = elevio_callButton(f, b);
                 for (int i = 0; i < 1000; ++i) {}
                 int btnPressedAfterDelay = elevio_callButton(f, b);
-                // Return true if the button is pressed and its state remained stable after the delay
+
                 if(btnPressed == 1 && btnPressed == btnPressedAfterDelay){
-                    elevio_buttonLamp(f, b, btnPressed);
-                    if(floor == f){
-                        elevio_motorDirection(DIRN_STOP);
-                        nanosleep(&(struct timespec){3, 0}, NULL);
-                        elevio_motorDirection(DIRN_UP);
+                    elevio_buttonLamp(f, b, 1);
+                    struct Request r;
+                    if(b == 2){
+                        r.floorTo = f;
+                        if(floor>f){
+                            r.direction = -1;
+                        }
+                        if(floor<f){
+                            r.direction = 1;
+                        }
                     }
-    
+
+                    if(floor == r.floorTo){
+                        elevio_motorDirection(DIRN_STOP);
+                        elevio_buttonLamp(f, b, 0);
+                        elevio_doorOpenLamp(1);
+                        nanosleep(&(struct timespec){3, 0}, NULL);
+                        elevio_doorOpenLamp(0);
+                        elevio_motorDirection(DIRN_UP);
+                        elevio_buttonLamp(f, b, 0);
+                    }
                 }
                 //elevio_buttonLamp(f, b, btnPressed);
             }
         }
 
-        
+
+
 
         if(elevio_obstruction()){
             elevio_stopLamp(1);
         } else {
             elevio_stopLamp(0);
         }
-        
+
         if(elevio_stopButton()){
             elevio_motorDirection(DIRN_STOP);
             break;
         }
-        
+
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
