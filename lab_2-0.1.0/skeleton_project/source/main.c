1cb4badaadbe26df7fb0044ae3a804911c4d62fb
selmaja <112476021+selmaja@users.noreply.github.com>
2024-03-09 20:31:48 +0100

    Update main.c

--- lab_2-0.1.0/skeleton_project/source/main.c
+++ lab_2-0.1.0/skeleton_project/source/main.c
@@ -60,14 +60,6 @@ int main(){
                                 for(int b = 0; b < N_BUTTONS; b++){
                                     printf("%d ", matrise[f][b]);
                                     
-                                    int btnPressed = elevio_callButton(f, b);
-                                    for (int i = 0; i < 1000; ++i) {}
-                                    int btnPressedAfterDelay = elevio_callButton(f, b);
-
-                                    if(btnPressed == 1 && btnPressed == btnPressedAfterDelay){
-                                        elevio_buttonLamp(f, b, 1);
-                                        matrise[f][b] = 1;
-                                    }
                                 }
                                 printf("\n");
                                
