c045829645efa7fcfb985efe3aa7845e90763115
Selmaja <selmaja@stud.ntnu.no>
2024-03-11 20:13:51 +0100

    bais

--- lab_2-0.1.0/skeleton_project/source/main.c
+++ lab_2-0.1.0/skeleton_project/source/main.c
@@ -32,9 +32,13 @@ void printMatrise(){
 
 void klokke(){
     clock_t start_time = clock();
-    printf("%d ", start_time);
+
     while ((clock() - start_time) / (double)CLOCKS_PER_SEC < 3.0) {
-        
+        for(int f = 0; f < N_FLOORS; f++){
+            for(int b = 0; b < N_BUTTONS; b++){
+                knapp(f, b);
+            }
+        }
     }
 }
 
